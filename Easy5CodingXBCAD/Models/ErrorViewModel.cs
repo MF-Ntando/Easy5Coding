70386c7ab1335cb18433a498d7cfc4ef4e556e5c
MF-Ntando <104993295+MF-Ntando@users.noreply.github.com>
2024-11-08 22:15:50 +0200

    Version 1

---
+++ Easy5CodingXBCAD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Easy5CodingXBCAD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
